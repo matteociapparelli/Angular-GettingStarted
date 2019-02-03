d13ff33fccb9a3a92b0c6a68a9e0a4a5b4dead9c
netarrow <netarrow89@hotmail.com>
2019-02-03 16:20:58 +0100

    implementato custom pipe per replace char con spazio. implementato filtro on typing su table

--- APM-Start/src/products/product-list.component.ts
+++ APM-Start/src/products/product-list.component.ts
@@ -1,19 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
 
 @Component({
   selector: 'pm-products',
-  templateUrl: './product-list.component.html'
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
   pageTitle = 'Product List';
   imageWidth = 50;
   imageMargin = 2;
 
   showImage = false;
 
-  listFilter = 'cart';
+  _listFilter = 'cart';
 
-  products: any[] = [
+  get listFilter(): string {
+    return this._listFilter;
+  }
+
+  set listFilter(value: string) {
+     this._listFilter = value;
+     this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+  }
+
+  filteredProducts: IProduct[];
+
+  products: IProduct[] = [
     {
 
       'productId': 1,
@@ -66,6 +79,21 @@ export class ProductListComponent {
       'imageUrl': 'https://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png'
     }
   ];
+
+constructor() {
+  this.filteredProducts = this.products;
+  this._listFilter = '';
+}
+
+  ngOnInit() {
+    console.log('On Init');
+  }
+
+performFilter(filterBy: string): IProduct[] {
+  filterBy = filterBy.toLocaleLowerCase();
+  return this.products.filter((product: IProduct) =>
+    product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+}
 
   toggleImage(): void {
     this.showImage = !this.showImage;
